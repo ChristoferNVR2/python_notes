10fdb28e314b3b320a708c44b14d021730c06d2f
ChristoferNVR2 <157941358+ChristoferNVR2@users.noreply.github.com>
2024-04-16 12:07:24 -0500

    Create 7. MATH FUNCTION.py

---
+++ 7. MATH FUNCTION.py
@@ -0,0 +1,16 @@
+import math
+
+pi = -3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi))
+print(math.ceil(pi))  # round up
+print(math.floor(pi))  # round down
+print(abs(pi))  # absolute value
+print(pow(pi, 2))  # power of
+print(math.sqrt(420))  # square root
+print(max(x, y, z))
+print(min(x, y, z))
+print(math.pi)  # constant
