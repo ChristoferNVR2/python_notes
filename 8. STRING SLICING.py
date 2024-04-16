d4d710283cf2e88c94746a05ee82487474e22cd8
ChristoferNVR2 <157941358+ChristoferNVR2@users.noreply.github.com>
2024-04-16 12:08:05 -0500

    Create 8. STRING SLICING.py

---
+++ 8. STRING SLICING.py
@@ -0,0 +1,13 @@
+# slicing = create a substring by extracting elements from another
+#           string indexing[] or slice()
+#           [start:stop:step]
+
+name = "Christofer Vega"
+
+first_name = name[:10]  # if start is not defined, it's 0
+last_name = name[11:]  # if stop is not defined, it goes to the end
+funky_name = name[::2]
+
+print(first_name)
+print(last_name)
+print(funky_name)
