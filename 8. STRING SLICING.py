aa61cb54c58ee1a7803d522feaa956be4ac51fde
ChristoferNVR2 <157941358+ChristoferNVR2@users.noreply.github.com>
2024-04-20 19:39:09 -0500

    Update 8. STRING SLICING.py

--- 8. STRING SLICING.py
+++ 8. STRING SLICING.py
@@ -11,3 +11,10 @@ funky_name = name[::2]
 print(first_name)
 print(last_name)
 print(funky_name)
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+slice_ = slice(7, -4)
+print(website1[slice_])
+print(website2[slice_])
