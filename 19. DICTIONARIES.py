889c06800ebd7a03353586db045f7ef5727148e8
ChristoferNVR2 <157941358+ChristoferNVR2@users.noreply.github.com>
2024-04-21 17:47:13 -0500

    Create 19. DICTIONARIES.py

---
+++ 19. DICTIONARIES.py
@@ -0,0 +1,21 @@
+# dictionary =  A changeable collection of unique key : value pairs
+# fast because they use hashing, allow us to access values quickly
+
+capitals = {"USA": "Washington DC",
+            "India": "New Delhi",
+            "China": "Beijing",
+            "Russia": "Moscow"}
+
+capitals.update({"Germany": "Berlin"})
+capitals.update({"USA": "Las Vegas"})
+capitals.pop("China")
+capitals.clear()
+
+print(capitals["USA"])
+print(capitals.get("Germany"))
+print(capitals.keys())
+print(capitals.values())
+print(capitals.items())
+
+for key, value in capitals.items():
+    print(key, value)
