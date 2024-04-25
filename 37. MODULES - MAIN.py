d12ed180d6c0fe1ecc8c4e6b76312a5d8ea5ceeb
ChristoferNVR2 <157941358+ChristoferNVR2@users.noreply.github.com>
2024-04-25 12:45:33 -0500

    Create 37. MODULES - MAIN.py

---
+++ 37. MODULES - MAIN.py
@@ -0,0 +1,9 @@
+# module = a file containing Python code. May contain functions, classes, etc.
+# used with modular programming, which is to separate a program into parts.
+
+import messages as msg
+# we can also use import messages and then use messages.hello() and messages.bye()
+# adding 'as msg' is just to make it shorter
+
+msg.hello()
+msg.bye()
