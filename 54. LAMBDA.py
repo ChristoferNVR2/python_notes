52323b9ce98a26885791076736c8847f41271bd3
ChristoferNVR2 <157941358+ChristoferNVR2@users.noreply.github.com>
2024-04-29 00:01:37 -0500

    Update 54. LAMBDA.py

--- 54. LAMBDA.py
+++ 54. LAMBDA.py
@@ -21,3 +21,20 @@ print(multiply(5, 6))
 print(add(5, 6, 7))
 print(full_name('Chris', 'Vega'))
 print(age_check(18))
+
+# Notice that pycharm give us a warning that we can replace lambda with a function
+# specifically "PEP 8: E731 do not assign a lambda expression, use a def"
+# This is a code style recommendation by PEP 8 that provide us recommendations
+# on how to write readable and consistent python code
+# In this specific case of warning E731, it suggests that we do not assign a lambda
+# expression to a variable and instead use a def to define a function (def) if the
+# function is complex enough to require a name, or simply use the lambda expression
+# directly where it is needed
+# for example
+# instead of
+# double = lambda x: x * 2
+# use
+# def double(x):
+#     return x * 2
+# or
+# print((lambda x: x * 2)(5))
